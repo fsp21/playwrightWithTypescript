39fc7ab29acf9095406e89b27dd9e471c1383845
fsp21 <felipeasaopaulo@gmail.com>
2024-08-03 23:32:55 -0300

    Adding comments to the code for easier readibility

--- specs/example.test.ts
+++ specs/example.test.ts
@@ -2,8 +2,13 @@ import { test, expect } from '@playwright/test';
 import * as testData from '../fixtures/testData.json';
 
 test('click test', async ({ page }) => {
+  // Opening URL specified in testData.json
   await page.goto(testData.exampleTestData.url);
+  
+  // Clicking "Calculate!" button to check result when no number has been passed
   await page.click('#getFactorial')
+  
+  // Check if text "Please enter an integer" appears in the div specified (id = resultDiv)
   const textLocator = page.locator('#resultDiv');
   await expect(textLocator).toBeVisible();
   const textContent = await textLocator.textContent();
