bd9a240ada4c1497a9dd3048455c6cd8ff691814
fsp21 <felipeasaopaulo@gmail.com>
2024-08-04 15:44:37 -0300

    Major Refactor and Two New Tests Added

--- specs/example.test.ts
+++ specs/example.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 import * as testData from '../fixtures/testData.json';
 
 test("Clicking 'Calculate!' without any inputs in text box", async ({ page }) => {
-  // Opening URL specified in testData.json
-  await page.goto(testData.exampleTestData.url);
+  // Navigate to the page
+  await page.goto(testData.objectData.url);
   
   // Clicking "Calculate!" button to check result when no number has been passed
   await page.click('#getFactorial')
@@ -13,15 +13,101 @@ test("Clicking 'Calculate!' without any inputs in text box", async ({ page }) =>
   await expect(textLocator).toBeVisible();
   const textContent = await textLocator.textContent();
   expect(textContent).toContain("Please enter an integer");
-})
-;
+});
 
-test('Check if page title matches value specified in testData.json', async ({ page }) => {
-  // Opening URL specified in testData.json
-  await page.goto(testData.exampleTestData.url);
+
+test("Check if page title matches value specified in testData.json", async ({ page }) => {
+  await page.goto(testData.objectData.url);
   
   // Checking if title matches value stored in testData.json file
   const pageTitle = await page.title();
-  expect(pageTitle).toEqual(testData.exampleTestData.expectedTitle);
-})
-;
+  expect(pageTitle).toEqual(testData.objectData.expectedTitle);
+});
+
+
+test('Checking API response after adding value 99999 and calculating', async ({ page }) => {
+  await page.goto(testData.objectData.url);
+
+  // Intercept the response
+ const [response] = await Promise.all([
+  page.waitForResponse(response => 
+    response.url().includes('/factorial')
+  ),
+
+  // Add a value to be calculated
+    await page.fill('#number', '99999'),
+
+  // Perform the action that triggers the request, e.g., clicking a button
+  page.click('#getFactorial')
+]);
+
+  // Check the status code of the intercepted response
+expect(response.status()).toBe(500);
+});
+
+
+test('Check API response for different numbers submitted', async ({ page }) => {
+  await page.goto(testData.objectData.url);
+
+  // Input values and expected responses:
+  const testCases = [
+    { input: '1', expected: 1 },
+    { input: '5', expected: 120 },
+    { input: '10', expected:3628800 },
+    { input: '11', expected:39916800 },
+  ];
+  
+  // Naming objects in the array as 'testCase' and declaring for loop:
+  for (const testCase of testCases) {
+        
+  // Wait for the API response and verify it after all steps have been completed:
+    const [response] = await Promise.all([
+      page.waitForResponse(response => 
+      response.url().includes('/factorial')
+    ),
+
+    // Fill the textbox with the input value
+    await page.fill('#number', testCase.input),
+    
+    // Click the button to trigger the API request
+    await page.click('#getFactorial')])
+  
+  // Parse the JSON response and declare expectation:
+    const responseBody = await response.json();
+    let finalValue = responseBody.answer
+    expect(finalValue).toEqual(testCase.expected);
+
+  // Log each value submitted, expected result and response:
+    console.log(`Input: ${testCase.input}, Expected: ${testCase.expected}, Received: ${finalValue}`);
+
+  }
+});
+
+
+test('Check if UI presents correct values retrieved from API', async ({ page }) => {
+  await page.goto(testData.objectData.url);
+
+  // Input values and expected responses:
+  const testCases = ['1', '5', '10', '11'];
+  
+  // Naming objects in the array as 'numberInput' and declaring for loop with its functions:
+  for (const numberInput of testCases) {
+    const [response] = await Promise.all([
+             page.waitForResponse(response => 
+             response.url().includes('/factorial')
+           ),
+     page.fill('#number', numberInput),
+     page.click('#getFactorial')
+    ]);
+
+  // Const and expectation declaration
+    const textLocator = page.locator('#resultDiv');
+    const textContent = await textLocator.textContent();
+    const responseBody = await response.json();
+    const finalValue = String(responseBody.answer);
+    expect(textContent).toContain(finalValue);
+    
+ // Logging expected and received values
+    console.log(`Input: ${numberInput}, Expected: The factorial of 1 is: ${finalValue}, Received: ${textContent}`);
+  }
+});
