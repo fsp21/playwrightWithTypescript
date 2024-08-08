80795b03a3e189ec7ff0ba24a9b8019b1af22408
fsp21 <felipeasaopaulo@gmail.com>
2024-08-08 20:33:49 -0300

    New test to practice importing and using interfaces

---
+++ specs/jsonPlaceholderTesting.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import * as apiTestData from '../fixtures/jsonPlaceHolder.json';
+import * as jsonPlaceholderServicesBody from '../fixtures/jsonPlaceholderServicesBody.ts';
+
+// Declare type of variable as array of objects declared in jsonPlaceholderServicesBody.ts;
+type jsonFormatResponse = jsonPlaceholderServicesBody.todos[]; 
+
+test("Practice importing and using interfaces with jsonPlaceHolder API", async ({ request }) => {
+
+    // Make the jsonPlaceHolder API request to "todos" service 
+    const response = await request.get(apiTestData.apiRequest.todosUrl);
+
+    // Return JSON of response using the interface imported from jsonPlaceholderServicesBody.ts;
+    const responseBody = await response.json() as jsonFormatResponse;
+
+    expect(responseBody.length).toBeGreaterThan(0);
+
+  });
