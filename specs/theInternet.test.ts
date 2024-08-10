1dac260dbe783b7b53dd1ea12c37b7b47a297b1a
fsp21 <felipeasaopaulo@gmail.com>
2024-08-10 15:59:32 -0300

    New tests added to verify login, refactoring older tests

--- specs/theInternet.test.ts
+++ specs/theInternet.test.ts
@@ -28,5 +28,72 @@ test("Check error if 'Login' is clicked without submitting username/password", a
     const alertContent = await page.locator('#flash').textContent();
     expect(alertContent).toContain('Your username is invalid!')
     
-    console.log('Content of alert banner: ' + alertContent);
+    console.log('Content of alert banner:  ' + alertContent);
+});
+
+// *** New Test ***
+test("Check error if 'Login' is clicked with a username but no password submitted", async ({ page }) => {
+    
+    await page.goto(theInternet.loginUrl);
+    expect(await page.isVisible('#login')).toBeTruthy();
+
+    await page.fill('#username', 'test');
+    await page.click('#login button');
+    expect(await page.isVisible('#flash')).toBeTruthy();
+
+    const alertContent = await page.locator('#flash').textContent();
+    expect(alertContent).toContain('Your username is invalid!');
+    
+    console.log('Content of alert banner:  ' + alertContent);
+});
+
+// *** New Test ***
+test("Check error if 'Login' is clicked with a password but no username submitted", async ({ page }) => {
+    
+    await page.goto(theInternet.loginUrl);
+    expect(await page.isVisible('#login')).toBeTruthy();
+
+    await page.fill('#password', 'test');
+    await page.click('#login button');
+    expect(await page.isVisible('#flash')).toBeTruthy();
+
+    const alertContent = await page.locator('#flash').textContent();
+    expect(alertContent).toContain('Your username is invalid!');
+    
+    console.log('Content of alert banner:  ' + alertContent);
+});
+
+// *** New Test ***
+test("Check message if 'Login' is clicked with correct password and username", async ({ page }) => {
+    
+    await page.goto(theInternet.loginUrl);
+    expect(await page.isVisible('#login')).toBeTruthy();
+
+    await page.fill('#username', 'tomsmith');
+    await page.fill('#password', 'SuperSecretPassword!');
+    await page.click('#login button');
+    expect(await page.isVisible('#flash')).toBeTruthy();
+
+    const alertContent = await page.locator('#flash').textContent();
+    expect(alertContent).toContain('You logged into a secure area!')
+    
+    console.log('Content of alert banner:  ' + alertContent);
+});
+
+// *** New Test ***
+test("Check flow after successful login and logout", async ({ page }) => {
+    
+    await page.goto(theInternet.loginUrl);
+    expect(await page.isVisible('#login')).toBeTruthy();
+
+    await page.fill('#username', 'tomsmith');
+    await page.fill('#password', 'SuperSecretPassword!');
+    await page.click('#login button');
+    expect(await page.isVisible('#content')).toBeTruthy;
+    expect(page.url()).toBe(theInternet.secureUrl);
+
+    await page.click('text="Logout"');
+    expect(await page.isVisible('#content h2')).toBeTruthy;
+    expect(page.url()).toBe(theInternet.loginUrl);
+
 });
