fb7ca8e9beb08a41825328ba1ba19b58f42b844c
fsp21 <felipeasaopaulo@gmail.com>
2024-08-10 11:29:16 -0300

    Refactoring one test from 'the internet' script and adding another one

--- specs/theInternet.test.ts
+++ specs/theInternet.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 import * as theInternet from "../fixtures/theInternet.json"
 
 // *** New Test ***
-test("The Internet Simple Automation", async ({ page }) => {
+test("'The Internet' website simple access and title check", async ({ page }) => {
     
     await page.goto(theInternet.baseUrl);
 
@@ -16,3 +16,17 @@ test("The Internet Simple Automation", async ({ page }) => {
     console.log('Expected Page Title to be: "' + theInternet.title + '", received: "' + title + '"');
 });
 
+// *** New Test ***
+test("Check error if 'Login' is clicked without submitting username/password", async ({ page }) => {
+    
+    await page.goto(theInternet.loginUrl);
+    expect(await page.isVisible('#login')).toBeTruthy();
+
+    await page.click('#login button');
+    expect(await page.isVisible('#flash')).toBeTruthy();
+
+    const alertContent = await page.locator('#flash').textContent();
+    expect(alertContent).toContain('Your username is invalid!')
+    
+    console.log('Content of alert banner: ' + alertContent);
+});
